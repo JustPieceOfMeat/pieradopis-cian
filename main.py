731103f23997ab790d036fd837325574ca79e3e6
Alexey Razumov <alexey_razumov@tutanota.com>
2020-11-30 22:12:42 +0300

    NSFW FILTER

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Standard python libs
 from os import environ
-from typing import List
+from typing import List, Iterable
 # apscheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 # Pyrogram
@@ -33,27 +33,44 @@ def check_updates():
             continue
         min_message_id = min([chat['lastMessageId'] for chat in channel['chats']])
         # noinspection PyTypeChecker
-        msg_ids = [m.message_id for m in user.iter_history(channel['_id'], offset_id=min_message_id, reverse=True)]
+        messages: List[Message] = \
+            [message for message in user.iter_history(channel['_id'], offset_id=min_message_id, reverse=True)
+             if not message.service]
+        msg_ids = [m.message_id for m in messages]
+        no_nsfw_msg_ids = [m.message_id for m in messages if 'NSFW' not in ' '.join([str(m.text), str(m.caption)])]
         if len(msg_ids) == 1:
             continue
         for chat in channel['chats']:
-            # noinspection PyTypeChecker
-            forwarded_messages: List[Message] = user.forward_messages(
-                environ.get('BOT_USERNAME'),
-                channel['_id'],
-                [msg_id for msg_id in msg_ids if msg_id > chat['lastMessageId']]
-            )
+            forwarded_messages: List[Message] = []
+            if chats.find_one({'_id': chat['chatId']})['NSFW']:
+                # noinspection PyTypeChecker
+                forwarded_messages = user.forward_messages(
+                    environ.get('BOT_USERNAME'),
+                    channel['_id'],
+                    [msg_id for msg_id in msg_ids if msg_id > chat['lastMessageId']]
+                )
+            elif len(no_nsfw_msg_ids) > 1:
+                # noinspection PyTypeChecker
+                forwarded_messages = user.forward_messages(
+                    environ.get('BOT_USERNAME'),
+                    channel['_id'],
+                    [msg_id for msg_id in no_nsfw_msg_ids if msg_id > chat['lastMessageId']]
+                )
             global bot_messages_ids
             while len(bot_messages_ids) != len(forwarded_messages):
                 pass
-            bot.forward_messages(
-                chat['chatId'],
-                user_id,
-                bot_messages_ids,
-                as_copy=chats.find_one({'_id': chat['chatId']})['asCopy']
-            )
-            bot_messages_ids = []
-            user.delete_messages(environ.get('BOT_USERNAME'), [message.message_id for message in forwarded_messages])
+            if len(bot_messages_ids) > 0:
+                bot.forward_messages(
+                    chat['chatId'],
+                    user_id,
+                    bot_messages_ids,
+                    as_copy=chats.find_one({'_id': chat['chatId']})['asCopy']
+                )
+                bot_messages_ids = []
+                user.delete_messages(
+                    environ.get('BOT_USERNAME'),
+                    [message.message_id for message in forwarded_messages]
+                )
             channels.update_one(
                 {
                     '_id': channel['_id'],
@@ -67,8 +84,13 @@ def check_updates():
             )
 
 
-@bot.on_message(filters.command(['start', f'start@{environ.get("BOT_USERNAME")}']))
+@bot.on_message(filters.command('start'))
 def on_start(client: Client, message: Message):
+    print('Вітаю, я працую толькі ў чатах. Дадай мяне ў любы чат і напішы /init каб праініцыялізаваць.')
+
+
+@bot.on_message(filters.command(['init', f'init@{environ.get("BOT_USERNAME")}']) & filters.group)
+def on_init(client: Client, message: Message):
     if message.chat.type in ('group', 'supergroup'):
         if message.from_user.id != 1423463788:
             if client.get_chat_member(message.chat.id, message.from_user.id).status not in ('administrator', 'creator'):
@@ -95,7 +117,7 @@ def on_start(client: Client, message: Message):
                       'Калі вы спрабуеце не першы раз — больш не спрабуйце, думаю, гэта не дапаможа.')
 
 
-@bot.on_message(filters.command(['link', f'link@{environ.get("BOT_USERNAME")}']))
+@bot.on_message(filters.command(['link', f'link@{environ.get("BOT_USERNAME")}']) & filters.group)
 def on_link(client: Client, message: Message):
     if message.chat.type in ('group', 'supergroup'):
         if message.from_user.id != 1423463788:
@@ -136,7 +158,7 @@ def on_link(client: Client, message: Message):
         message.reply('Не атрымалася знайсці такі канал.')
 
 
-@bot.on_message(filters.command(['channels', f'channels@{environ.get("BOT_USERNAME")}']))
+@bot.on_message(filters.command(['channels', f'channels@{environ.get("BOT_USERNAME")}']) & filters.group)
 def on_channels(client: Client, message: Message):
     if message.chat.type in ('group', 'supergroup'):
         if message.from_user.id != 1423463788:
@@ -150,7 +172,7 @@ def on_channels(client: Client, message: Message):
     sent_message.edit_text('Спіс прывязанных каналаў:\n' + '\n'.join(['@' + name for name in channel_usernames]))
 
 
-@bot.on_message(filters.command(['unlink', f'unlink@{environ.get("BOT_USERNAME")}']))
+@bot.on_message(filters.command(['unlink', f'unlink@{environ.get("BOT_USERNAME")}']) & filters.group)
 def on_unlink(client: Client, message: Message):
     if message.chat.type in ('group', 'supergroup'):
         if message.from_user.id != 1423463788:
@@ -189,11 +211,15 @@ def generate_settings_markup(chat_id) -> InlineKeyboardMarkup:
         [InlineKeyboardButton(
             f"Капіяваць: {'Так' if chat['asCopy'] else 'Не'}",
             'dont_copy' if chat['asCopy'] else 'copy'
-        )]
+        )],
         # [InlineKeyboardButton(
         #     f"Дадаваць подпіс: {'Так' if chat['asCopy'] else 'Не'}",
         #     'dont_sign' if chat['sign'] else 'sign'
-        # )]
+        # )],
+        [InlineKeyboardButton(
+            f"NSFW: {'Так' if chat['NSFW'] else 'Не'}",
+            'dont_send_NSFW' if chat['NSFW'] else 'send_NSFW'
+        )],
     ])
 
 
@@ -219,6 +245,16 @@ def on_callback(_: Client, callback: CallbackQuery):
                 }
             }
         )
+    elif callback.data == 'send_NSFW':
+        chats.update_one(
+            {'_id': callback.message.chat.id},
+            {'$set': {'NSFW': True}}
+        )
+    elif callback.data == 'dont_send_NSFW':
+        chats.update_one(
+            {'_id': callback.message.chat.id},
+            {'$set': {'NSFW': False}}
+        )
     callback.message.edit_reply_markup(generate_settings_markup(callback.message.chat.id))
 
 
@@ -231,6 +267,6 @@ if __name__ == '__main__':
     bot.start()
     user.start()
     scheduler = BackgroundScheduler()
-    scheduler.add_job(check_updates, "interval", seconds=60)
+    scheduler.add_job(check_updates, "interval", seconds=5)
     scheduler.start()
     idle()
